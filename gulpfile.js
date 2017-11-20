dd0b38a1d73385404fd80cd6b5595ac0741faa11
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-11-19 17:53:12 -0800

    Update gulp and add watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,34 @@ const gulp = require('gulp');
 const panini = require('panini');
 const sass = require('gulp-sass');
 
-gulp.task('default', ['sass', 'panini']);
+gulp.task('default', gulp.series(gulp.parallel(pages, style), watch));
 
-gulp.task('sass', function() {
+function pages() {
+  return gulp.src('./panini/pages/**/*.html')
+             .pipe(panini({
+               root: './panini/pages/',
+               layouts: './panini/layouts/',
+               partials: './panini/partials',
+               helpers: './panini/helpers',
+               data: './panini/data'
+             }))
+             .pipe(gulp.dest('.'));
+}
+
+function style() {
   var bootstrapSrc = "./node_modules" + bootstrap._location + "/scss";
   return gulp.src('./sass/**/*.scss')
             .pipe(sass({includePaths: [bootstrapSrc]}).on('error', sass.logError))
             .pipe(gulp.dest('./css'));
-});
+}
 
-gulp.task('panini', function () {
-  gulp.src('./panini/pages/**/*.html')
-      .pipe(panini({
-        root: './panini/pages/',
-        layouts: './panini/layouts/',
-        partials: './panini/partials',
-        helpers: './panini/helpers',
-        data: './panini/data'
-      }))
-      .pipe(gulp.dest('.'));
-});
+function resetPages(done) {
+  panini.refresh();
+  done();
+}
+
+function watch() {
+  gulp.watch('./panini/pages/**/*.html').on('all', gulp.series(pages));
+  gulp.watch('./panini/{layouts, partials}/**/*.html').on('all', gulp.series(resetPages, pages));
+  gulp.watch('./sass/**/*.scss').on('all', gulp.series(style));
+}
